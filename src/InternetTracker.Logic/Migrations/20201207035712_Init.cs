91da539314533f44a97519305e74b01d69daf356
Neville <live.neville@live.com>
2020-12-06 21:15:01 -0700

    added function yaml

---
+++ src/InternetTracker.Logic/Migrations/20201207035712_Init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InternetTracker.Logic.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FailedLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    TimeStamp = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Elapsed = table.Column<double>(type: "double", nullable: false),
+                    ErrorMessage = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FailedLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FailedLogs_TimeStamp",
+                table: "FailedLogs",
+                column: "TimeStamp");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FailedLogs");
+        }
+    }
+}
